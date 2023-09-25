a003bda55ef386dd3ecb5f8222d2e95f57ddd42b
Hunter Pittman <hunterpittman@protonmail.com>
2023-09-25 12:51:42 -0700

    Muh new routes bruh

---
+++ ciso-next/src/app/(user)/officers/page.tsx
@@ -0,0 +1,14 @@
+import BasicCard from "@/components/OfficersCard";
+
+export default function Officers()
+{
+    return (
+        <>
+            <h1>Yuh we doin it</h1>
+
+            <div className='flex items-center justify-center'>
+                <BasicCard/>
+            </div>
+        </>
+    );
+}
